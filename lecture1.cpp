19cf94696850472c78ef649be80c53681d89c24f
Sameer Singh <Sameer0Singh123@gmail.com>
2024-03-09 02:36:01 +0530

    First lecture is finished

---
+++ lecture1.cpp
@@ -0,0 +1,61 @@
+// This bring the iostream library
+#include <iostream>
+using namespace std;
+
+// function for adding two number
+// int addNumber(int number1, int number2){
+//    int sum = number1 + number2;
+//    return sum;
+
+// }
+
+// // function for the multiplication of two number
+// int multiplyNumber(int number1, int number2){
+//   int multiple = number1 * number2;
+//   return multiple;
+// }
+int main()
+{
+
+    /* what are comments ?
+      comments in programming languages are used to explain code
+    they can be single line or multi-line
+    for the single  line comment we use two slashes // and anything
+    and for the multiline comment we use the  */
+
+    // cout<<"Hello world";
+    
+    // compile time
+    // cout<<"This is the compile time error example"
+    
+    // Runtime error
+    // cout<<20/0;  this is the runtime error
+    //  our program can terminate by itself  or crash 
+    // see this we should try
+    // cout<<"hello world";
+    // int value = 7/0;
+    // cout<<value;
+    
+    // our program have terminated before line 24
+
+    // statement and function
+
+    /*statement ==> Statements are the basic  unit of c++ program.
+    A program in the c++ is the collection of statements and statement in order to
+    execute from the top to bottom and they keep executing until a statement causing to_string
+     termninate the program*/
+
+   /* function ==> Function are the block of code to perfom a specific task and return value when
+    set of arguments are passed in it. The function must be defined before using it
+    */
+
+    // int number1 = 55;
+    // int number2 = 855;
+    // int sum = addNumber(number1, number2);
+    // cout<<"The sum of two number " << number1 << " and " << number2 <<" is "<< sum;
+
+    // cout<<" The multiplication of two number" << 52 << " and " << 65 << " is " << multiplyNumber(52, 65) ;
+     
+    
+     return 0;
+}
